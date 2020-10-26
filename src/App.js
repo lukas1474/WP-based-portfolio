83c86d9b349342a7e0fc923fce1565c18ccfb4f9
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-10-26 20:06:33 +0100

    init project and create file structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+// import logo from './logo.svg';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+import PortfolioMain from './components/views/PortfolioMain/PortfolioMain';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      {/*<img src={logo} className="App-logo" alt="logo" />*/}
+      <p>
+        PORTFOLIO/ miejsce na nagłówek i logo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <a
+        className="App-link"
+        href="https://www.duraj-wnetrza.pl/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Strona główna
         </a>
-      </header>
-    </div>
-  );
-}
+    </header>
+    <body className="App-body">
+      <PortfolioMain />
+    </body>
+    <footer className="App-footer">
+      2020
+    </footer>
+  </div>
+);
+
+App.propTypes = {
+  children: PropTypes.node,
+};
 
 export default App;
