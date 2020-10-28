2db4875f9692d2d4a95ad11a36ca1ad6aae55210
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-10-28 12:02:57 +0100

    add simple list with categories

--- src/components/views/PortfolioMain/PortfolioMain.js
+++ src/components/views/PortfolioMain/PortfolioMain.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import './PortfolioMain.scss';
+import styles from './PortfolioMain.scss';
+import PropTypes from 'prop-types';
 
 class PortfolioMain extends React.Component {
   render() {
     return (
       <div className="App">
+        <div className='container'>
+          <div className={styles.panelBar}>
+            <div className={styles.menu}>
+              <ul>
+                <li>
+                  <p>
+                    Wszystkie
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    Elewacja
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    Salon
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    kuchnia
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    Łazienka
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
         <h2>Lista projektów z kategoriami</h2>
         <div>
           <p>projekty</p>
@@ -14,5 +48,9 @@ class PortfolioMain extends React.Component {
     );
   }
 }
+
+PortfolioMain.propTypes = {
+  children: PropTypes.node,
+};
 
 export default PortfolioMain;
