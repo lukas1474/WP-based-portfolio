1f44469a87213fb0f7b0227e91b2278f5db9f89c
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-10-28 22:01:21 +0100

    remove unnecessary code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,14 @@
 import {combineReducers, createStore} from 'redux';
-import initialState from './initialState';
 import categoriesRedux from './categoriesRedux';
 
 const reducers ={
   categories: categoriesRedux,
 };
 
-Object.keys(initialState).forEach(item => {
-  if (typeof reducers[item] == 'undefined') {
-    reducers[item] = (statePart = null) => statePart;
-  }
-});
-
 const combinedReducers = combineReducers(reducers);
 
 const store = createStore(
   combinedReducers,
-  initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
